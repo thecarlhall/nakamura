a4be95f0e54f3e7464870430381704f564b94a21
Carl Hall <carl@hallwaytech.com>
2012-07-05 19:40:47 -0700

    Set default values for properties on media listener.

--- contrib/media/common/src/main/java/org/sakaiproject/nakamura/media/MediaListenerImpl.java
+++ contrib/media/common/src/main/java/org/sakaiproject/nakamura/media/MediaListenerImpl.java
@@ -60,19 +60,19 @@ import org.slf4j.LoggerFactory;
 })
 public class MediaListenerImpl implements MediaListener, EventHandler, FileUploadHandler {
   static final int MAX_RETRIES_DEFAULT = 5;
-  @Property
+  @Property(intValue = MAX_RETRIES_DEFAULT)
   public static final String MAX_RETRIES = "maxRetries";
 
   static final int RETRY_MS_DEFAULT = 5 * 60 * 1000;
-  @Property
+  @Property(intValue = RETRY_MS_DEFAULT)
   public static final String RETRY_MS = "retryMs";
 
   static final int WORKER_COUNT_DEFAULT = 5;
-  @Property
+  @Property(intValue = WORKER_COUNT_DEFAULT)
   public static final String WORKER_COUNT = "workerCount";
 
   static final int POLL_FREQUENCY_DEFAULT = 5000;
-  @Property
+  @Property(intValue = POLL_FREQUENCY_DEFAULT)
   public static final String POLL_FREQUENCY = "pollFrequency";
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MediaListenerImpl.class);
