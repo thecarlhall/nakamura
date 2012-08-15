85c0c096d40a35fed7924a5c07c7c663bd552b9f
Mayte Giménez <la.reina.roja@gmail.com>
2012-08-16 00:58:43 +0200

    Added another servlet to listen OAuthVerifier

---
+++ bundles/oauthDemo/src/main/java/org/sakaiproject/nakamura/oauthDemo/OAuthDemoVerifyServlet.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.sakaiproject.nakamura.oauthDemo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+
+@SlingServlet(methods={"GET"}, paths={"/system/sling/oauthDemoVerifier"})
+@Properties(value={
+	    @Property(name = "service.description", value = "The Sakai Foundation"),
+	    @Property(name = "service.vendor", value = "The Sakai Foundation") 
+})
+
+public class OAuthDemoVerifyServlet extends SlingAllMethodsServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.getWriter().write("Hello World from Oauth Server: " );
+
+	}
+}
